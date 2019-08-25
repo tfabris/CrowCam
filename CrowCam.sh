b9b6deed84c3c212d63e41757ce92163a918044d
Tony Fabris <tfabris@gmail.com>
2019-08-25 13:09:44 -0700

    Increase number of retries for stream checking test.
    Attempt to reduce the number of times per day that the stream gets reset when Comcast is being flaky (as it has been the last week or two).

--- CrowCam.sh
+++ CrowCam.sh
@@ -94,10 +94,10 @@ PauseBetweenTests=10
 # Try to prevent busting the quota by re-querying more slowly. You can view
 # your quota usage here:
 # https://console.developers.google.com/apis/api/youtube.googleapis.com/quotas?project=crowcam
-NumberOfStreamTests=4
+NumberOfStreamTests=5
 
 # Number of seconds to pause between stream-up-check tests.
-PauseBetweenStreamTests=40
+PauseBetweenStreamTests=45
 
 # When in test mode, pause for a shorter period between tests, and do fewer
 # loops of the main network test loop.
