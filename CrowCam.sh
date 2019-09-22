ea40d2e396152f5a16454337b6a81aa94323d6e7
Tony Fabris <tfabris@gmail.com>
2019-09-22 11:36:44 -0700

    Increase hysteresis on stream check.
    Attempt to fix issue of too-frequent reboots due to the "Video Ingestion Faster Than Realtime" error. The error occurs legitimately sometimes but then after a successful reboot I suspect it hangs around and cries wolf in the YouTube API. See if adding additional hysteresis fixes it.

--- CrowCam.sh
+++ CrowCam.sh
@@ -94,10 +94,10 @@ PauseBetweenTests=10
 # Try to prevent busting the quota by re-querying more slowly. You can view
 # your quota usage here:
 # https://console.developers.google.com/apis/api/youtube.googleapis.com/quotas?project=crowcam
-NumberOfStreamTests=5
+NumberOfStreamTests=7
 
 # Number of seconds to pause between stream-up-check tests.
-PauseBetweenStreamTests=45
+PauseBetweenStreamTests=49
 
 # When in test mode, pause for a shorter period between tests, and do fewer
 # loops of the main network test loop.
